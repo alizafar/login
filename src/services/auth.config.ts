88e1bfbd58a641079afcd28629d72a2dac22cda1
Ali Zafar <ali.zafar@iridium-technology.com>
2018-06-16 23:11:36 +1000

    Second Commit

---
+++ src/services/auth.config.ts
@@ -0,0 +1,10 @@
+// Change this filename to auth.config.ts
+
+export const AUTH_CONFIG = {
+  // Needed for Auth0 (capitalization: ID):
+  clientID: 'g6o4WP7bRqSysVXBHCLR3ITzj0IY6VSc',
+  // Needed for Auth0Cordova (capitalization: Id):
+  clientId: 'g6o4WP7bRqSysVXBHCLR3ITzj0IY6VSc',
+  domain: 'alizafar.au.auth0.com', // e.g., you.auth0.com
+  packageIdentifier: 'com.auth0.ionic' // config.xml widget ID
+};
